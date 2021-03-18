6566bc6b8e68a20e131433f073bd9f6ad24ccf2d
Yohenba18 <roddik12kshetri@gmail.com>
2021-03-18 21:29:41 +0530

    quiz v1.3

--- src/App.js
+++ src/App.js
@@ -3,8 +3,21 @@ import './App.css';
 
 function App() {
   return (
-    <div className="quiz">
-      <h1 style={{"color": "white"}}>Quiz</h1>
+    <div className="App">
+      <div className="quiz">
+        <div className="quiz__header">
+            <div className="quiz__headerLeft">
+                <span>Questions</span>
+            </div>
+            <div className="quiz__headerRight">
+                <span id="id1">1/</span>
+                <span id="id5">5</span>
+            </div>
+        </div>
+        <div className="quiz__ques">
+          <h3><span>1.</span> Who is the richest person in the world?</h3>
+        </div>
+      </div>
     </div>
   );
 }
