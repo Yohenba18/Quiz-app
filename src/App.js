49b1823eb569d13f81db53a20b89e42bc39fe650
Yohenba18 <roddik12kshetri@gmail.com>
2021-04-02 18:47:27 +0530

    quiz v4.0 responsive

--- src/App.js
+++ src/App.js
@@ -19,14 +19,14 @@ function App() {
         </div>
 
         <div className="quiz__ans">
-        <div className="quiz__top">
-        <div className="options col-3"><button>Jeff Bezos</button></div>
-            <div className="options col-3"><button>Elon Musk</button></div>
-        </div>
-        <div className="quiz__bottom">
-        <div className="options col-3"><button>Bill Gates</button></div>
-            <div className="options col-3"><button>Mark Zukerberg</button></div>
-        </div>
+
+        <div className="options"><button>Jeff Bezos</button></div>
+            <div className="options"><button>Elon Musk</button></div>
+
+
+        <div className="options"><button>Bill Gates</button></div>
+            <div className="options"><button>Mark Zukerberg</button></div>
+
             
             
         </div>
