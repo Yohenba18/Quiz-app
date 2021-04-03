a03ad210185768ae97e2f91c9a6904bee0244db2
Yohenba18 <roddik12kshetri@gmail.com>
2021-04-03 13:39:02 +0530

    quiz v5.0 making dynamic

--- src/App.js
+++ src/App.js
@@ -1,38 +1,46 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
 
 function App() {
+   
+  
+
   return (
     <div className="App">
       <div className="quiz">
         <div className="quiz__header">
-            <div className="quiz__headerLeft">
-                <span>Questions</span>
-            </div>
-            <div className="quiz__headerRight">
-                <span id="id1">1/</span>
-                <span id="id5">5</span>
-            </div>
+          <div className="quiz__headerLeft">
+            <span>Questions</span>
+          </div>
+          <div className="quiz__headerRight">
+            <span id="id1">1/</span>
+            <span id="id5">5</span>
+          </div>
         </div>
         <div className="quiz__ques">
-          <h3><span>1.</span> Who is the richest person in the world?</h3>
+          <h3>
+            <span>1.</span> Who is the richest person in the world?
+          </h3>
         </div>
 
         <div className="quiz__ans">
+          <div className="options">
+            <button>Jeff Bezos</button>
+          </div>
+          <div className="options">
+            <button>Elon Musk</button>
+          </div>
 
-        <div className="options"><button>Jeff Bezos</button></div>
-            <div className="options"><button>Elon Musk</button></div>
-
-
-        <div className="options"><button>Bill Gates</button></div>
-            <div className="options"><button>Mark Zukerberg</button></div>
-
-            
-            
+          <div className="options">
+            <button>Bill Gates</button>
+          </div>
+          <div className="options">
+            <button>Mark Zukerberg</button>
+          </div>
         </div>
       </div>
       <footer>
-       <p>Made with 💖 by Yohenba Kshetrimayum</p>
+        <p>Made with 💖 by Yohenba Kshetrimayum</p>
       </footer>
     </div>
   );
